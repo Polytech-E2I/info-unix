88d2cce00874a49354910be43c35d765fa20b2ca
Robin SHAMSNEJAD <rsham@free.fr>
2022-01-07 08:24:55 +0100

    Add exo01

---
+++ TP06_01/longestword.py
@@ -0,0 +1,8 @@
+
+
+user_input = input("Entrez plusieurs mots : ")
+
+strings = user_input.split(" ")
+lengths = [len(string) for string in strings]
+
+print("Le mot le plus long fait {} caractères.".format(max(lengths)))
