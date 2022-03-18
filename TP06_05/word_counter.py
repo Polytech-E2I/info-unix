89d9abe16310434eefbd494e757210af0a74b7bb
Robin SHAMSNEJAD <rsham@free.fr>
2022-03-18 09:05:10 +0100

    Update word counter

--- TP06_05/word_counter.py
+++ TP06_05/word_counter.py
@@ -1,5 +1,12 @@
+from pprint import pprint
+
 string = " je crois ce que je dis et je fais ce que je crois "
 word_list = string.strip().split(" ")
 string_dict = {item:word_list.count(item) for item in word_list}
 
-print(f"{string_dict=}")
+string_dict_tab = []
+
+for item in string_dict:
+    string_dict_tab.append({item: string_dict[item]})
+
+pprint(string_dict_tab)
